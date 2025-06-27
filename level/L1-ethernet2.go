216f2cd44a8623bf608f2abafa9b9eb68a08007d
xuyang <206465633@qq.com>
2025-06-27 12:04:49 +0800

    L1-EthernetII complete

--- level/L1-ethernet2.go
+++ level/L1-ethernet2.go
@@ -85,117 +85,83 @@ func NewEthernet2(dMacAddress, sMacAddress [6]byte, protocolTypeS string,
 }
 
 // Serialize 将以太网帧序列化为[]byte
+// @author xuyang
+// @datetime 2025/6/27 11:00
 // @return []byte 序列化后的字节数组
 func (e *Ethernet2) Serialize() []byte {
-	// 计算总长度：头部(14字节) + 数据包长度 + CRC(4字节)
 	totalSize := EthernetHeaderSize + len(e.DataPackage) + 4
 	result := make([]byte, totalSize)
-
 	offset := 0
-
-	// 复制目的MAC地址
 	copy(result[offset:], e.DMacAddress[:])
 	offset += 6
-
-	// 复制源MAC地址
 	copy(result[offset:], e.SMacAddress[:])
 	offset += 6
-
-	// 复制协议类型
 	copy(result[offset:], e.ProtocolType[:])
 	offset += 2
-
-	// 复制数据包
 	copy(result[offset:], e.DataPackage)
 	offset += len(e.DataPackage)
-
-	// 复制CRC校验和
 	copy(result[offset:], e.CRCCheckSum[:])
-
 	return result
 }
 
 // Deserialize 将[]byte反序列化为以太网帧
+// @author xuyang
+// @datetime 2025/6/27 11:00
 // @param data 要反序列化的字节数组
 // @return *Ethernet2 反序列化后的以太网帧，如果失败返回nil
 func Deserialize(data []byte) *Ethernet2 {
-	// 检查数据长度是否足够
-	if len(data) < EthernetHeaderSize+4 {
+	if len(data) < EthernetHeaderSize+MinDataSize+4 {
+		// panic("数据长度不足，数据包可能不是ErhernetII报文")
 		return nil
 	}
-
 	frame := &Ethernet2{}
 	offset := 0
-
-	// 解析目的MAC地址
 	copy(frame.DMacAddress[:], data[offset:offset+6])
 	offset += 6
-
-	// 解析源MAC地址
 	copy(frame.SMacAddress[:], data[offset:offset+6])
 	offset += 6
-
-	// 解析协议类型
 	copy(frame.ProtocolType[:], data[offset:offset+2])
 	offset += 2
-
-	// 解析数据包（除去CRC的剩余部分）
 	dataSize := len(data) - EthernetHeaderSize - 4
-	if dataSize > 0 {
-		frame.DataPackage = make([]byte, dataSize)
-		copy(frame.DataPackage, data[offset:offset+dataSize])
-		offset += dataSize
-	}
-
-	// 解析CRC校验和
+	frame.DataPackage = make([]byte, dataSize)
+	copy(frame.DataPackage, data[offset:offset+dataSize])
+	offset += dataSize
 	copy(frame.CRCCheckSum[:], data[offset:offset+4])
-
 	return frame
 }
 
 // ValidateCRC 检测以太网帧的CRC校验和是否正确
+// @author xuyang
+// @datetime 2025/6/27 12:00
 // @return bool 如果CRC正确返回true，否则返回false
 func (e *Ethernet2) ValidateCRC() bool {
-	// 计算当前数据的CRC
 	calculatedCRC := e.calculateCRC()
-
-	// 比较计算出的CRC和存储的CRC
 	return calculatedCRC == e.CRCCheckSum
 }
 
 // generateCRC 生成CRC校验和
+// @author xuyang
+// @datetime 2025/6/27 12:00
 func (e *Ethernet2) generateCRC() {
 	e.CRCCheckSum = e.calculateCRC()
 }
 
 // calculateCRC 计算CRC校验和
+// @author xuyang
+// @datetime 2025/6/27 12:00
 // @return [4]byte 计算出的CRC校验和
 func (e *Ethernet2) calculateCRC() [4]byte {
-	// 创建用于CRC计算的数据（不包括CRC字段本身）
 	crcData := make([]byte, EthernetHeaderSize+len(e.DataPackage))
-
 	offset := 0
-
-	// 复制目的MAC地址
 	copy(crcData[offset:], e.DMacAddress[:])
 	offset += 6
-
-	// 复制源MAC地址
 	copy(crcData[offset:], e.SMacAddress[:])
 	offset += 6
-
-	// 复制协议类型
 	copy(crcData[offset:], e.ProtocolType[:])
 	offset += 2
-
-	// 复制数据包
 	copy(crcData[offset:], e.DataPackage)
-
-	// 使用CRC32计算校验和
 	crc := crc32.ChecksumIEEE(crcData)
-
 	var result [4]byte
 	binary.BigEndian.PutUint32(result[:], crc)
-
 	return result
 }
