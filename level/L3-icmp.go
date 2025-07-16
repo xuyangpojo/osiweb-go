ff5e9b30fd09dcf1bff71388fdcfe38cdb234811
xuyangpojo <xuyangpojo@icloud.com>
2025-07-16 22:22:33 +0800

    windows 7.16

--- level/L3-icmp.go
+++ level/L3-icmp.go
@@ -1,1 +1,32 @@
 package level
+
+// @author xuyang
+// @datetime 2025/7/15 23:00
+type ICMP struct {
+	Type byte
+	Code byte
+	CheckSum [2]byte
+	MessageBody []byte
+}
+
+// @author xuyang
+// @datetime 2025/7/15 23:00
+func NewICMP(stype string) *ICMP {
+	itype := 0
+	code := 0
+	// ping请求
+	if stype == "pingRequest" {
+		itype = 8
+		code = 0
+	}
+	// ping应答
+	if stype == "pingResponse" {
+		itype = 0
+		code = 0
+	}
+	// TODO: CheckSum校验和计算
+	newICMP := &ICMP{
+		Type: itype,
+		Code: code,
+	}
+}
